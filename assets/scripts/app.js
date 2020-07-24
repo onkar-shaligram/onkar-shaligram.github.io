8dd84b6aa6fb86c5ea9388f2d41e4750fd8d5c67
onkar-shaligram <onkarshaligram01@gmail.com>
2020-07-25 02:22:47 +0530

    Enabled the on-hover effect in particle.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -82,7 +82,7 @@ particlesJS('particles-js',
       }
     },
     "interactivity": {
-      "detect_on": "canvas",
+      "detect_on": "window",
       "events": {
         "onhover": {
           "enable": true,
